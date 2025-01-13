297bf73499da6866256310eee035762d1006d7cd
GitEngHar <harukii1999@gmail.com>
2025-01-13 18:41:06 +0900

    QuizFormを削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import QuizMain from './pages/QuizMain';
-import QuizForm from './pages/QuizForm';	
 import QuizDetails from './pages/QuizDetails';
 import QuizDetailsForm from './pages/QuizDetailsForm';	
 import TakeQuiz from './pages/TakeQuiz';
@@ -15,7 +14,6 @@ const App: React.FC = () =>{
     <Router>
       <Routes>
         <Route path="/" element={<QuizMain />}></Route>
-        <Route path="/quiz/form" element={<QuizForm />}></Route>
         <Route path="/quizdetails" element={<QuizDetails />}></Route>
         <Route path="/quizdetails/form" element={<QuizDetailsForm />}></Route>
         <Route path="/quizdetails/edit" element={<QuizDetailsEditForm />}></Route>
