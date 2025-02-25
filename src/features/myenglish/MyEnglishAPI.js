e12e710b4eb4f34d0095f96158e70730e388dec7
GitEngHar <harukii1999@gmail.com>
2025-02-26 08:52:14 +0900

    login confirm

--- src/features/myenglish/MyEnglishAPI.js
+++ src/features/myenglish/MyEnglishAPI.js
@@ -1,29 +1,27 @@
 import axios from 'axios';
 // myenglish-server-service
-// const requestQuestionTitleBaseUrl =  'http://localhost:8080/quizrest';
-// const requestQuestionDetailsBaseUrl = "http://localhost:8080/quizdetailsrest";
-// const requestTakeQuestionBaseUrl = "http://localhost:8080/takequizrest/";
-// const authLoginUrl = "http://localhost:8080/login"
+const requestQuestionTitleBaseUrl =  'http://localhost:8080/quizrest';
+const requestQuestionDetailsBaseUrl = "http://localhost:8080/quizdetailsrest";
+const requestTakeQuestionBaseUrl = "http://localhost:8080/takequizrest/";
+const requestLoginConfirmBaseUrl = "http://localhost:8080/login/confirm";
+const authLoginUrl = "http://localhost:8080/login"
 
-const requestQuestionTitleBaseUrl =  'http://myenglish-server-service:8080/quizrest';
-const requestQuestionDetailsBaseUrl = "http://myenglish-server-service:8080/quizdetailsrest";
-const requestTakeQuestionBaseUrl = "http://myenglish-server-service:8080/takequizrest/";
-const authLoginUrl = "http://myenglish-server-service:8080/login"
+// const requestQuestionTitleBaseUrl =  'http://myenglish-server-service:8080/quizrest';
+// const requestQuestionDetailsBaseUrl = "http://myenglish-server-service:8080/quizdetailsrest";
+// const requestTakeQuestionBaseUrl = "http://myenglish-server-service:8080/takequizrest/";
+// const authLoginUrl = "http://localhost:8082/login"
 axios.defaults.withCredentials = true;
 
 
-function redirectBackendServer(error){
-    console.log(error);
-    console.log(error.response);
+export function redirectBackendServer(){
     window.location.href = authLoginUrl;
-    console.log(error)
 }
 
 export const questionTitleSave = async (questionTitle) => {
     try{
         return await axios.post(requestQuestionTitleBaseUrl+'/save', questionTitle);
     }catch (error){
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -32,7 +30,7 @@ export const questionTitleUpdate = async (newQuestionTitle) => {
     try{
         return await axios.post(requestQuestionTitleBaseUrl+'/update', newQuestionTitle);
     }catch (error){
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -42,7 +40,7 @@ export const questionTitleGet = async ()=> {
         const response = await axios.get(requestQuestionTitleBaseUrl)
         return response.data;
     }catch (error){
-        redirectBackendServer(error);
+        //redirectBackendServer();
         throw error;
     }
 }
@@ -51,7 +49,7 @@ export const questionTitleDelete = async (questionTitle) => {
     try{
         return await axios.post(requestQuestionTitleBaseUrl+'/delete', questionTitle);
     }catch (error){
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -60,7 +58,7 @@ export const questionDetailsAdd = async (questionDetailsWrapper) => {
     try {
         return await axios.post(requestQuestionDetailsBaseUrl + '/save', questionDetailsWrapper);
     } catch (error) {
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -70,7 +68,7 @@ export const questionDetailsUpdate = async (questionDetailsWrapper) => {
     try {
         return await axios.post(requestQuestionDetailsBaseUrl + '/update', questionDetailsWrapper);
     } catch (error) {
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -80,7 +78,7 @@ export const questionDetailsEdit = async (questionDetails) => {
         const response = await axios.post(requestQuestionDetailsBaseUrl + '/edit', questionDetails);
         return response.data;
     } catch (error) {
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -90,7 +88,7 @@ export const questionDetailsGet = async (questionTitle) => {
         const response = await axios.post(requestQuestionDetailsBaseUrl + "/all", questionTitle);
         return response.data;
     } catch (error) {
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -99,7 +97,7 @@ export const questionDetailsDelete = async (details) => {
     try {
         return await axios.post(requestQuestionDetailsBaseUrl + '/delete', details);
     } catch (error) {
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
@@ -109,11 +107,19 @@ export const takeQuizGet = async (questionTitle) => {
         const response = await axios.post(requestTakeQuestionBaseUrl, questionTitle);
         return response.data;
     } catch (error) {
-        redirectBackendServer(error);
+        redirectBackendServer();
         throw error;
     }
 }
 
-
+export const loginConfirmGet = async () => {
+    try {
+        const response = await axios.get(requestLoginConfirmBaseUrl);
+        return response.data;
+    } catch (error) {
+        redirectBackendServer();
+        throw error;
+    }
+}
 
 
