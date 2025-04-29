07010086d7d062f02319a989b0251a420150110e
GitEngHar <harukii1999@gmail.com>
2025-04-29 12:11:52 +0900

    create package & add system value object

---
+++ src/domain/QuizDetails.ts
@@ -0,0 +1,7 @@
+import isEqual from 'lodash/isEqual';
+
+export class QuizDetails{
+    private readonly _questionDetailsID: number;
+    private readonly _questionTitleID: number;
+    private readonly _questionTitleID: number;
+}
