b74d072385cd56e58e2688846f468c01790b6016
GitEngHar <harukii1999@gmail.com>
2025-05-08 09:14:08 +0900

    検証ように向き先を切り替え

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import QuizMain from '../pages/QuizMain';
 import QuizTitle from '../pagesv2/QuizTitle';
-import QuizDetails from '../pages/QuizDetails';
+import QuizDetails from '../pagesv2/QuizDetails';
+// import QuizDetails from '../pages/QuizDetails';
 import TakeQuiz from '../pages/TakeQuiz';
 import AIQuiz from "../pages/AIQuiz";
 import '../style.css';
