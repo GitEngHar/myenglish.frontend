339794590ed8c8a9099cb220cfddaf933b08a73e
GitEngHar <harukii1999@gmail.com>
2025-05-18 17:23:00 +0900

    warn修正

--- src/application/quiztry/QuizTryJudgeService.ts
+++ src/application/quiztry/QuizTryJudgeService.ts
@@ -2,6 +2,6 @@ import {QuizDetailsDTO} from "../../dto/QuizDetailsDTO";
 
 export class QuizTryJudgeService {
     collectAnswer(userAnswer: number, quizDetails: QuizDetailsDTO): boolean {
-        return userAnswer == quizDetails.answerNumber;
+        return userAnswer === quizDetails.answerNumber;
     }
 }
